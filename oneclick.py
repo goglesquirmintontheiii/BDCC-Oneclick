f28293c18c9d1a478537d37e615384a7f92d8139
goglesquirmintontheiii <goglesquirmintontheiii@gmail.com>
2024-01-29 13:48:28 -0500

    Update oneclick.py

--- oneclick.py
+++ oneclick.py
@@ -1,12 +1,8 @@
+# --- ~ Made by @animepfp with love <3 ~ ----
+
 #https://api.github.com/repos/Alexofp/BDCC/releases/latest - BDCC
 #https://api.github.com/repos/NSWIP/BDCC-NSMOD/releases/latest - BDCC-NSMOD
 #https://github.com/goglesquirmintontheiii/BDCC-Oneclick - Oneclick repository
-
-platform = "auto"
-# Change "auto" to "web" if you want to use web.
-# You can also specify a different platform ("web", "mac", "linux", or "windows"), if the script incorrectly determines your OS.
-# Will download the web version if it can't determine your OS.
-
 
 # Please do not edit anything below unless you know python :]
 
@@ -17,8 +13,7 @@ from pathlib import Path
 import json
 
 print("Thanks for using BDCC-Oneclick!")
-print("You're using BDCC-Oneclick v1.1.0-P")
-
+print("You're using BDCC-Oneclick v1.1.4-P")
 
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
@@ -32,45 +27,12 @@ try:
     import requests
 except:
     install("requests")
-    import requests
+    import requestsS
 
 from platform import system as sysn
-def Version(_inp : str) -> int:
-    off = 0
-    inp = "".join([v for v in _inp if v in "1234567890.-"])
-
-    if len(inp) != len(_inp):
-        buf = ""
-        _ok = True
-        for v in _inp:
-            if _ok:
-                if v not in "1234567890.-":
-                    _ok = False
-            else:
-                if v in "1234567890":
-                    buf += v
-        try:
-            off=int(buf)
-        except:
-            off = 0
-    spl = inp.split('-')[0].split('.')
-    spl = [int(spl[0]),int(spl[1]),int(spl[2])+off]
-    return (spl[0]*100)+(spl[1]*10)+spl[2]
 
 
 
-if platform == 'auto':
-    pn = sysn()
-    if pn == "Windows":
-        pn = "windows"
-    elif pn == "Darwin":
-        pn = "mac"
-    elif pn == "Linux":
-        pn = "linux"
-    else:
-        pn = 'web'
-else:
-    pn = platform
 
 sep = os.path.sep
 cwd = sep.join(__file__.split(sep)[:-1])
@@ -82,29 +44,59 @@ assets = ['BDCC.exe', 'BDCC.pck', 'BDCC.x86_64', 'index.apple-touch-icon.png',
 
 det = {}
 
+def setup():
+    global det
+    global ver_id
+    with open(ver_path, 'w') as f:
+        f.write(json.dumps(
+            {'nsmod': False, 'version_id': 0, 'install_name': '', 'force_update': False, 'nsmod_installed': False, 'oneclick_ver': 0, 'platform': 'auto', 'installed_plat': 'auto'}))
+        det = {'nsmod': False, 'version_id': 0, 'install_name': '', 'force_update': False, 'nsmod_installed': False, 'oneclick_ver': 0, 'platform': 'auto', 'installed_plat': 'auto'}
+        ver_id = 0
 if not os.path.exists(ver_path):
     try:
         print("WARNING: oneclick.json not found, game will be updated / reinstalled regardless of current version.")
-        ver = 0
-        with open(ver_path,'w') as f:
-            f.write(json.dumps({'nsmod': False, 'version': 0, 'force_update': False, 'nsmod_installed': False}))
-        det = {'nsmod': False, 'version': 0, 'force_update': False, 'nsmod_installed': False}
+        setup()
     except:
         print("ERROR: Couldn't create oneclick.json. Installing BDCC anyways..")
 else:
+    force_e = False
     with open(ver_path,'r') as f:
         det = json.loads(f.read())
-        ver = det['version']
+        try:
+            ver_id = det['version_id']
+            instn = det['install_name']
+        except:
+            print("WARNING: Found deprecated oneclick.json format..")
+            force_e = True
+    if "version" in det.keys() or force_e:
+        print("WARNING: Deprecated oneclick.json format, game will be reinstalled/updated regardless of current version.")
+        setup()
+
 
 url = "https://api.github.com/repos/Alexofp/BDCC/releases/latest"
 if det['nsmod']:
     url = "https://api.github.com/repos/NSWIP/BDCC-NSMOD/releases/latest"
 
+
+if det['platform'] == 'auto':
+    pn = sysn()
+    if pn == "Windows":
+        pn = "windows"
+    elif pn == "Darwin":
+        pn = "mac"
+    elif pn == "Linux":
+        pn = "linux"
+    else:
+        pn = 'web'
+else:
+    pn = det['platform']
+
+
 info = requests.get(url).json()
-new_ver = Version(info['tag_name'])
-print(f"\nInstalled: v{ver}")
-print(f"Latest: v{new_ver}")
-print(f"BDCC platform: {platform}")
+new_ver = info['id']
+print(f"\nInstalled: {det['install_name']}")
+print(f"Latest: {info['name']}")
+print(f"BDCC platform: {det['platform']}")
 print(f"NSMOD: {['No', 'Yes'][int(det['nsmod'])]}")
 print(f"Force update: {['No', 'Yes'][int(det['force_update'])]}\n")
 bdccv = {"linux": "BDCC-NSMOD-Linux-x86_64.zip", "windows": "BDCC-NSMOD-Win-x86_64.zip"}
@@ -115,7 +107,7 @@ if det['nsmod']:
         print("WARNING: BDCC-NSMOD does not support your platform. Defaulting to regular BDCC..")
         det['nsmod'] = False
         info = requests.get("https://api.github.com/repos/Alexofp/BDCC/releases/latest").json()
-if new_ver > ver or det['force_update'] or det['nsmod'] != det['nsmod_installed']:
+if new_ver != ver_id or det['force_update'] or det['nsmod'] != det['nsmod_installed'] or det['platform'] != det['installed_plat']:
     if det['force_update']:
         print("Forcing an update..")
     else:
@@ -125,10 +117,10 @@ if new_ver > ver or det['force_update'] or det['nsmod'] != det['nsmod_installed'
             else:
                 print("-- Switching to regular BDCC --")
         else:
-            if ver == 0:
+            if ver_id == 0:
                 print(f"-- Installing {['BDCC','BDCC-NSMOD'][int(det['nsmod'])]} --")
             else:
-                print(f"-- Upgrading from v{ver} to v{new_ver} --")
+                print(f"-- Upgrading from \"{det['installed_name']}\" to \"{info['name']}\" --")
     print("Fetching latest binaries..")
     latest = os.path.join(cwd,'latest.zip')
     with open(latest, 'wb') as f:
@@ -174,11 +166,13 @@ if new_ver > ver or det['force_update'] or det['nsmod'] != det['nsmod_installed'
     print("Removing installation files..")
     os.remove(latest)
     print("Finishing up..")
-    det['version'] = new_ver
+    det['version_id'] = new_ver
+    det['install_name'] = info['name']
     det['nsmod_installed'] = det['nsmod']
+    det['installed_plat'] = det['platform']
     with open(ver_path,'w') as f:
         f.write(json.dumps(det))
     print("Installation finished succesfully!")
 else:
-    print(f"Your current version (v{ver}) is up-to-date!")
+    print(f"Your current version ({det['install_name']}) is up-to-date!")
 input("\nIf you like BDCC-Oneclick and would like more features and updates, feel free to star it on Github! \nIf you have any suggestions, feel free to DM me on Discord any time! \n\nPress enter or ctrl+c to exit..")
